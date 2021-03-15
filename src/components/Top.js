d1185e8293b8cddf5be9c8461038d218656c710a
mandeep-kafle <mandeepkaflee.work@gmail.com>
2021-03-15 18:50:09 +0530

    final commit

--- src/components/Top.js
+++ src/components/Top.js
@@ -19,12 +19,12 @@ function Top() {
                
                 <Tooltip title="View Profile on Linkedin">
                     <IconButton aria-label="f">
-                    <LinkedInIcon color="primary" fontSize="large" onClick={event =>  window.open('https://www.facebook.com/mandeep.kafle')}/>
+                    <LinkedInIcon color="primary" fontSize="large" onClick={event =>  window.open('https://www.linkedin.com/in/mandeep-kafle/')}/>
                     </IconButton>
                 </Tooltip>
                 <Tooltip title="View code on github">
                     <IconButton aria-label="Follow me on facebook">
-                    <GitHubIcon color="action" fontSize="large" onClick={event =>  window.open('https://www.facebook.com/mandeep.kafle')}/>
+                    <GitHubIcon color="action" fontSize="large" onClick={event =>  window.open('https://github.com/mandeep-kafle/mandeepkafle')}/>
                     
                     </IconButton>
                 </Tooltip>
